4bc27ef5809013d0d73ca977071a8b1f3e3d6e8a
Amanda Mabunda <amandamabu14@gmail.com>
2024-05-07 01:04:45 +0200

    task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -14,4 +14,26 @@ $(document).ready(function (){
 })
 
 // grab the items checked from the button in the html
-// add the to th post request
+// add the to  post request
+
+$(document).ready(function () {
+ $("button").click(function() {
+  var selectedAmenities = [];
+  $("input[type='checkbox']:checked").each(function() {
+    selectedAmenities.push($(this).val());
+  });
+
+  $.post("http://0.0.0.0:5001/api/v1/places_search/",
+        {},
+        function(data) {
+          let places = data;
+          console.log(data);
+          $.each(places, function(value) {
+                $("section.places").textContent("<article>"
+                    + value["name"] + "</article>"
+          });
+        },
+         "json"
+        );
+    });
+});
